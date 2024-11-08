2f17d6f8dc04ba246fe24b121ebb687dd8a62aa5
Emma Winston <emma.winston+brighter@gmail.com>
2024-11-08 21:08:49 +0000

    remove preview text

--- training/info/js/script.js
+++ training/info/js/script.js
@@ -1,5 +1,5 @@
 // Add the buttons to convert to image
-  document.getElementById("image").innerHTML = "<p>If you're happy, click the button to turn your table into an image you can save.<br/><button id='renderFullImage'>Screenshot table</button><div id='previewImage'>the preview image</div><div id='downloadImage'></div>";
+  document.getElementById("image").innerHTML = "<p>If you're happy, click the button to turn your table into an image you can save.<br/><button id='renderFullImage'>Screenshot table</button><div id='previewImage'></div><div id='downloadImage'></div>";
   
   document.getElementById("renderFullImage").addEventListener('click', function(){snapshot("mytable")}, false);
 
